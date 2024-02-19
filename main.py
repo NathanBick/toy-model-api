22a223cc59d82f209d0bc452c40af2c0ea767178
Nathan Bick <nathankbick1@gmail.com>
2024-02-19 16:55:15 -0500

    inference using fastAPI on mlflow model

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import mlflow.sklearn
+import mlflow
+import pickle
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import numpy as np
 from typing import List
+
+# set mlflow tracking uri
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 app = FastAPI()
 
@@ -9,8 +16,19 @@ app = FastAPI()
 class Item(BaseModel):
     data: List[List[float]]
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 @app.post("/predict/")
 async def make_prediction(item: Item):
+    # read in the model
+    # TODO: avoid always reading the model every time a request is made
+    # try:
+    model = mlflow.sklearn.load_model("toy_model")
+    # except:
+    #     model = pickle.load(open("toy_model.pkl", "rb"))
+
     try:
         data = np.array(item.data)
         prediction = model.predict(data)
