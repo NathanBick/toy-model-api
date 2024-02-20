f3b14a233f9fb42edc24c7f68611ebe92f923269
Nathan Bick <nathankbick1@gmail.com>
2024-02-20 18:46:27 -0500

    Add inference to Azure ML service endpoint

--- predict.py
+++ predict.py
@@ -1,12 +1,19 @@
 import requests
 
-# inference data
+# inference data with names
 data = {
     "data": [[0.00632,18.00,2.310,0,0.5380,6.5750,65.20,4.0900,1,296.0,15.30,396.90,4.98]]
 }
 
-# make request
+# make request to local fastapi server
 response = requests.post("http://127.0.0.1:8000/predict/", json=data)
+
+# print prediction
+print(response)
+print(response.text)
+
+# make request to azure ml service deployed model endpoint
+azure_response = requests.post("https://test-model-hnbss.eastus2.inference.ml.azure.com/score", json=data)
 
 # print prediction
 print(response)
